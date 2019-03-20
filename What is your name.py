7d732eb1a6c359a44cd474d3ac5621d03e10c0e8
JOHN NIKOLAIDIS <102512769@student.swin.edu.au>
2019-03-20 14:39:20 +1100

    Created File

---
+++ What is your name.py
@@ -0,0 +1,6 @@
+print("Let's learn about you!")
+name = input("What is your name?: ")
+place = input("Where do you live?: ")
+pet = input("What is your pets name?: ")
+
+print("Your name is " + name + " and you are from " + place + ". Your pets name is " + pet + " and boy is it ugly.")
