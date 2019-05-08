6f0f50a8eba1659a48eb3ad49d770dbb8d3c32f3
JOHN NIKOLAIDIS <102512769@student.swin.edu.au>
2019-05-08 16:49:39 +1000

    comit

---
+++ projectEuler/problem004.py
@@ -0,0 +1,24 @@
+# amountofdigits = int(input("How many digits do you want to check for palindromes?: "))
+# startingRangeInput = ""
+# palindromeList = []
+
+# def palindromeChecker(palindromeStr):
+#     palindromeList = palindromeStr.split("")
+#     print(palindromeList)
+
+breakupword = "Break me up"
+testvalue = len(breakupword) - 1
+print(testvalue)
+if testvalue > 0:
+    print(breakupword[testvalue])
+    testvalue = testvalue - 1
+
+# # print(palindromeChecker("987654321"))
+
+# for startingRangeCount in range(amountofdigits):
+#     startingRangeInput = startingRangeInput + "9"
+
+# for firstMultiple in range(int(startingRangeInput),0,-1):
+#     secondMultiple = firstMultiple
+#     if secondMultiple > 0:
+#         break
